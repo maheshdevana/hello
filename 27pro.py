eb6e2fc57678435d6b1e347d22ee5a9b992e19b5
maheshdevana <36931457+maheshdevana@users.noreply.github.com>
2019-07-29 22:39:51 +0530

    Create 27pro.py

---
+++ 27pro.py
@@ -0,0 +1,34 @@
+#a
+o,k=map(int,input().split())
+
+h=list(map(int,input().split()))
+
+s=list(map(int,input().split()))
+
+t=[]
+
+c=0
+
+for i in range(o):
+
+    x=s[i]/h[i]
+
+    t.append(x)
+
+while k>=0 and len(t)>0:
+
+    mindex=t.index(max(t))
+
+    if k>=p[mindex]:
+
+        c=c+s[mindex]
+
+        k=k-h[mindex]
+
+    h.pop(mindex)
+
+    s.pop(mindex)
+
+    t.pop(mindex)
+
+print(c)
